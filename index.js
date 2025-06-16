9bf7bf191931b21ff29c8c3bd3f654a28c2aa620
fgatti <fgattidelaconcpecion@gmail.com>
2025-06-16 20:00:23 -0300

    Preparando archivo para servidor

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config(); 
 
 const express = require("express");
+const cors = require("cors");
 const admin = require("firebase-admin");
 const app = express();
 const PORT = process.env.PORT || 5000; 
